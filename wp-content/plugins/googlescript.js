e8c3725b43977935d603c075b3786bad10c0739f
drtrailer2022 <114238381+drtrailer2022@users.noreply.github.com>
2023-08-18 17:10:11 +0530

    updated final

---
+++ wp-content/plugins/googlescript.js
@@ -0,0 +1,17 @@
+// googlescript.js
+
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-CGHFT7B4FR';
+  
+    document.head.appendChild(script);
+  
+    script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-CGHFT7B4FR');
+    };
+  })();
+  
